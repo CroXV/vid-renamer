fc2cc8262cabe28abe8b1efde4a01531de73da32
CroXV <CroXV001@gmail.com>
2019-08-20 20:19:08 +0530

    added decorators, added a way to exit script

--- rename/config.py
+++ rename/config.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+from rename.app import ExitProgram
 import json
 import logging
 # logging.basicConfig(filename='config.log', level=logging.DEBUG,
@@ -5,46 +8,50 @@ import logging
 
 
 class Config:
-    # TODO: raise error if value is equal to Q
-    def __init__(self, key, value=None):
+    # check if exit conditions are met
+    value = ExitProgram()
+
+    def __init__(self, key):
         self.key = key
-        self.value = value
 
         self.file = 'config.json'
         self.data = self.load_file()
 
     def load_file(self):
-        # tries to load json file and return it.
+        # try to load json file and return it.
         # if json file does not exist,
-        # it makes a new json file and returns an empty dictionary.
+        # make a new json file and return an empty dictionary.
         try:
             data = {}
             with open(self.file) as file:
                 data = json.load(file)
         except FileNotFoundError:
-            logging.INFO('Creating new json file.')
+            logging.info('Creating new json file...')
             with open(self.file, 'w') as file:
                 json.dump(data, file)
         finally:
             return data
 
+    def save_value(self, value):
+        # update value
+        self.value = value
+        # set value in loaded data and update json file with new data.
+        self.data[self.key] = value
+        with open(self.file, 'w') as file:
+            json.dump(self.data, file)
+        return value
+
+    @property
     def load_value(self):
+        # try to get value from loaded data if key exists.
         try:
             return self.data[self.key]
         except KeyError:
-            logging.INFO('Key not found.')
+            logging.warning('Key not found.')
 
-    def save_value(self):
-        # set value in loaded data, and update json file with new data.
-        self.data[self.key] = self.value
-        with open(self.file, 'w') as file:
-            json.dump(self.data, file)
-
-        return self.value
-
-    def del_key(self):
-        # remove key from dictionary
+    @load_value.deleter
+    def load_value(self):
+        # remove key from dictionary and update json file.
         self.data.pop(self.key, None)
-
         with open(self.file, 'w') as file:
             json.dump(self.data, file)
