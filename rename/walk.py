8cc9beebc069bf281dd1e84bf88969c0e9be94a0
CroXV <CroXV001@gmail.com>
2019-08-22 19:59:16 +0530

    renamed walk_dir.py to walk.py

---
+++ rename/walk.py
@@ -0,0 +1,30 @@
+from rename import db
+from rename import config
+from rename.regex import regex_name
+import os
+
+
+def walk_dir(directory):
+    # go through all files in folder and store them in database with a new name
+    for foldername, subfolders, files in os.walk(directory):
+        # save current folder name to database
+        db.set('foldername', foldername)
+
+        for file in files:
+            name = regex_name(file)
+            filename = os.path.join(foldername, file)
+            new_filename = os.path.join(foldername, name)
+
+            db.add(filename, new_filename)
+        config.del_value('name')
+
+        for subfolder in subfolders:
+            name = regex_name(subfolder)
+            subfolder = os.path.join(foldername, subfolder)
+            new_subfolder = os.path.join(foldername, name)
+
+            db.set('foldername', subfolder)
+            db.add(subfolder, new_subfolder)
+
+    db.update()
+    config.update()
