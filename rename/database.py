0e05ca6450cb881f5676ecda2c57766d06780949
CroXV <CroXV001@gmail.com>
2019-08-23 14:44:00 +0530

    added print database method

--- rename/database.py
+++ rename/database.py
@@ -1,3 +1,6 @@
+import os
+
+
 class NameDatabase:
     def __init__(self):
         self.database = {}
@@ -16,11 +19,24 @@ class NameDatabase:
         return self.database[key]
 
     def update(self):
-        # removes same key value and script set keys from instance database
+        # removes same key value and set keys from instance database
         updated_database = {}
-        for k, v in self.database.items():
-            if k != v and k not in self.own_keys:
-                updated_database[k] = v
+        if self.database:
+            for k, v in self.database.items():
+                if k != v and k not in self.own_keys:
+                    updated_database[k] = v
 
         self.database = updated_database
 
+    def print_database(self):
+        if self.database:
+            for k, v in self.database.items():
+                k = os.path.basename(k)
+                v = os.path.basename(v)
+                print(f'\t{k} --> {v}')
+        else:
+            raise NoNameError('\tNo Files to rename.')
+
+
+class NoNameError(Exception):
+    pass
