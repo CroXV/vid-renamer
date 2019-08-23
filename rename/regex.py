fd13b943ae83dc7096340c7bd366cb5f886c36b8
CroXV <CroXV001@gmail.com>
2019-08-23 14:42:55 +0530

    removed try except

--- rename/regex.py
+++ rename/regex.py
@@ -1,4 +1,4 @@
-from rename.settings.app_descriptor import EmptyStringError
+
 import rename.parser as parser
 import re
 import os
@@ -15,16 +15,13 @@ def regex_name(name):
         r'(.*?)[-.(_\s](?!1[0-7]\d{2}|\d{4}p)([12]\d{3})').search(name)
 
     # parse name and return
-    try:
-        if anime:
-            return f'{parser.parse_episode(anime.group(2))}{ext}'
-        elif digits:
-            return f'{parser.parse_episode(digits.group())}{ext}'
-        elif series:
-            return f'{parser.parse_series(series.group())}{ext}'
-        elif movie:
-            return f'{parser.parse_movie(movie.group(1), movie.group(2))}{ext}'
-        else:
-            return f'{name}{ext}'
-    except (TypeError, EmptyStringError):
+    if anime:
+        return f'{parser.parse_episode(anime.group(2))}{ext}'
+    elif digits:
+        return f'{parser.parse_episode(digits.group())}{ext}'
+    elif series:
+        return f'{parser.parse_series(series.group())}{ext}'
+    elif movie:
+        return f'{parser.parse_movie(movie.group(1), movie.group(2))}{ext}'
+    else:
         return f'{name}{ext}'
