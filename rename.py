6b56d5fb98ace37f453ce6542486b578de298a09
CroXV <CroXV001@gmail.com>
2019-08-23 14:45:37 +0530

    code cleanup

--- rename.py
+++ rename.py
@@ -1,12 +1,38 @@
+from rename.database import NoNameError
 from rename.walk import walk_dir
-from rename.path import get_directory
+from rename.path import get_path
+from rename import db
+import os
+import sys
 
 
 def renamer():
-    print('(Enter Q to exit script.)')
-    directory = get_directory()
+    print('\nPress (Y/N) to continue...')
+    while True:
+        confirm = input('> ').lower()
+
+        if confirm == 'q' or confirm == 'n':
+            print('\nClosing script...')
+            sys.exit()
+        elif confirm == 'y':
+            for k, v in db.database.items():
+                os.rename(k, v)
+            print('\nDone')
+            break
+
+
+def main():
+    print('(Press Q to exit script.)')
+    directory = get_path()
     walk_dir(directory)
+
+    print('\nRenaming:')
+    try:
+        db.print_database()
+        renamer()
+    except NoNameError as err:
+        print(err)
 
 
 if __name__ == '__main__':
-    renamer()
+    main()
